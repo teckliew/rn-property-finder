7d9c5c00271de2ce097227e52cc75d879fd6a1be
teckliew <bboytek@gmail.com>
2016-03-09 15:17:04 -0800

    git init

---
+++ index.ios.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+} from 'react-native';
+const SearchPage = require('./search-page')
+
+class PropertyFinder extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}></NavigatorIOS>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80,
+  },
+  container: {
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Property Finder React-Native iOS app
+---
+
+resource: http://www.raywenderlich.com/99473/introducing-react-native-building-apps-javascript
